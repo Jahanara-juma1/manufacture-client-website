5c37bfd470f3528ba4cfd14a530e36c765b6cfcc
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-05-22 23:20:56 +0600

    primary button addad

--- src/Page/Home/Contact.js
+++ src/Page/Home/Contact.js
@@ -1,14 +1,13 @@
 import React from "react";
-// import appointment from '../../assets/images/appointment.png';
-// import PrimaryButton from "../Shared/PrimaryButton";
+import PrimaryButton from "../Shared/PrimaryButton";
 
 const Contact = () => {
   return (
     <div style={{
-        // background:`url(${appointment})`
+        
     }} className='bg-neutral px-10 py-10 '>
       <div className='text-center pb-14 text-white'>
-        <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
+        <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent'>
           Contact Us
         </p>
         <h1 className='text-4xl'>Stay connected with us</h1>
@@ -29,7 +28,7 @@ const Contact = () => {
           placeholder='Your message'
           rows={6}
         ></textarea>
-        {/* <PrimaryButton>Submit</PrimaryButton> */}
+        <PrimaryButton>Submit</PrimaryButton>
       </div>
     </div>
   );
